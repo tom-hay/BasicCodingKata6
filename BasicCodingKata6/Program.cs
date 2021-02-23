511791cfda3c9e870d976c8b62ff8fe3a64c17b6
Tom Haywood <tom.haywood@myob.com>
2021-02-23 13:53:31 +1100

    got program to function correctly

---
+++ BasicCodingKata6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicCodingKata6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // user inputs number
+            Console.WriteLine("Please choose a number");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            // user decides whether they want sum or product
+            var message = string.Format("Would you like to see the sum, or the product of 1 to {0}?", input);
+            Console.WriteLine(message);
+            Console.WriteLine("Please type sum then press enter, or type product then press enter");
+            var sumOrProduct = Console.ReadLine();
+
+            // create a list from a range of 1..input
+            var numbers = Enumerable.Range(1, input);
+
+            // sum all items in the list
+            var sum = numbers.Sum();
+
+            // multiply all items in the list
+            int product = 1;
+            foreach (int number in numbers)
+                product *= number;
+
+            // execute the chosen operation
+            if (sumOrProduct == "sum")
+                Console.WriteLine(sum);
+            else
+                Console.WriteLine(product);
+        }
+    }
+}
